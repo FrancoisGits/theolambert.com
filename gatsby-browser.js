b0b238d9080fbd6ad5d0cd4e013864948cc68a35
Sonicfury <theo.lambert@viacesi.fr>
2020-12-13 18:02:35 +0100

    feat(styles): implemented dark mode

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,14 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-// You can delete this file if you're not using it
+const html = document.querySelector('html');
+
+exports.onInitialClientRender = () => {
+  const sessionPreference = sessionStorage.getItem('theme');
+  const userPrefers = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+
+  if (sessionPreference){
+    html.setAttribute('theme', sessionPreference);
+  } else if (userPrefers && !sessionPreference){
+    html.setAttribute('theme', userPrefers);
+  } else {
+    html.setAttribute('theme', 'light');
+  }
+}
